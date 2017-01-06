ef1b0ae61792be11a8ed31c225ad8d03d1334b57
mandyreal <normandy.real@gmail.com>
2017-01-06 23:49:15 +0800

    Added Weather API and display of location and weather details

--- 02/weather/js/app.js
+++ 02/weather/js/app.js
@@ -21,8 +21,33 @@ function getLocation(position) {
   	$.getJSON(url, function(response) {
     city = response.results[0].address_components[2].short_name;
     country = response.results[0].address_components[3].long_name;
-    $('#location').attr('value', city + ", " + country);
+    $('#location').text(city + ", " + country);
     console.log(city + "," + country);
+    getWeather(city, country);
 	});
+
+function getWeather(city, country) {
+	var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country +'&appid=e4c8a9da719d414b907f7a31035009b1&units=metric';
+//	var url = 'http://api.openweathermap.org/data/2.5/weather?q=New York,USA&appid=e4c8a9da719d414b907f7a31035009b1&units=metric';  	
+
+  	$.getJSON(url, function(response) {
+  	var hr = (new Date()).getHours(); 
+  	var day_night;
+  	if (hr >= 5 && hr <= 17) {
+  		day_night = "day-"
+  	} else {
+  		day_night = "night-"
+  	};
+  	console.log("Hour of day : " + hr + " " + day_night);	
+  	console.log("OWM icon : " + response.weather[0].id);
+    $('#icon').removeClass();
+    $('#icon').addClass('wi wi-owm-' + day_night + response.weather[0].id);
+//	$('#icon').addClass('wi wi-night-clear');
+	console.log("OWM temp : " + response.main.temp);
+	console.log("OWM desc : " + response.weather[0].description);
+    $('#temperature').text(response.main.temp + '  °C');
+    $('#description').text(response.weather[0].description);
+	});
+};
 
 };

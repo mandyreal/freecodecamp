ad2b0bcfaac62d998466e3ecce881c90f42be9bd
Mandy Real <normandy.real@gmail.com>
2017-01-16 22:48:02 +1100

    added wikipedia API call to wiki viwere

--- 02/wiki/js/app.js
+++ 02/wiki/js/app.js
@@ -1,4 +1,24 @@
-$(document).ready(function() {
-  console.log("New app")
-});
+function callWiki() {
+	title = document.getElementById("query").value;
+	apiUrl = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="
+	           + title + "&callback=?";
+//	console.log(apiUrl);
+	resultTitle = [];
+	resultDesc = [];
+	resultID = [];
+	$.getJSON(apiUrl, function(json){
+		$.each(json.query.pages, function(index, item) {
+			resultTitle.push(item.title);
+			resultDesc.push(item.extract);
+			resultID.push(item.pageid);
+		});	
+	});
+	console.log(resultTitle);
+}
 
+$(document).ready(function() {
+  $("#submit").on("submit", function(event) {
+	event.preventDefault();
+    callWiki();
+  });
+});

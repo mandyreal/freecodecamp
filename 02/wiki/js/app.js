4b95bd09d0fb24efc64959c31443c7aa3b8c3591
Mandy Real <normandy.real@gmail.com>
2017-01-17 22:13:44 +1100

    added display of results

--- 02/wiki/js/app.js
+++ 02/wiki/js/app.js
@@ -2,23 +2,33 @@ function callWiki() {
 	title = document.getElementById("query").value;
 	apiUrl = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="
 	           + title + "&callback=?";
-//	console.log(apiUrl);
-	resultTitle = [];
-	resultDesc = [];
-	resultID = [];
 	$.getJSON(apiUrl, function(json){
 		$.each(json.query.pages, function(index, item) {
-			resultTitle.push(item.title);
-			resultDesc.push(item.extract);
-			resultID.push(item.pageid);
+            var pageId=item.pageid;
+            var title=item.title;
+            var extract=item.extract;
+            var link="http://en.wikipedia.org/wiki/?curid="+pageId;
+            var html="<div class='well'><a href="+link+" target='_blank'><h2>"+title+"</h2><p>"+extract+"</p></a></div>";
+             $('.container-narrow').append(html);			
 		});	
 	});
-	console.log(resultTitle);
 }
 
 $(document).ready(function() {
-  $("#submit").on("submit", function(event) {
-	event.preventDefault();
-    callWiki();
-  });
+  	$("#submit").on("submit", function(event) {
+		event.preventDefault();
+		// Reset container-narrow before displaying results
+		$('.container-narrow').empty();
+    	callWiki();
+  	});
+
+	$('#search').keypress(function(e){
+         if(e.which==13){
+            searchTerm=$('#query').val();
+            console.log(searchTerm);
+            // Reset container-narrow before displaying results
+            $('.container-narrow').empty();
+            callWiki();
+         };
+    });
 });

0a3e5f6142f2ae75a51a60117347092eec764405
Mandy Real <normandy.real@gmail.com>
2017-01-21 22:26:35 +1100

    add tabs to Twitch

---
+++ algorithm scripting/dates.js
@@ -0,0 +1,10 @@
+//Convert a date range consisting of two dates formatted as YYYY-MM-DD into a more readable format.
+
+
+function makeFriendlyDates(arr) {
+  var temp = arr[0].split('-');
+  console.log(temp);
+  return arr;
+}
+
+makeFriendlyDates(['2016-07-01', '2016-07-04']);

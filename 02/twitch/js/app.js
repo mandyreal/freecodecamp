369664de073d401fd1fae45de86bafb6b57c1b9c
Mandy Real <normandy.real@gmail.com>
2017-01-22 21:44:58 +1100

    Added call to API

--- 02/twitch/js/app.js
+++ 02/twitch/js/app.js
@@ -1,2 +1,15 @@
+var channels = ["freecodecamp"]
+
+function callApi() {
+	var url = "https://wind-bow.gomix.me/twitch-api/streams/";
+
+	channels.forEach(function(channel){
+		$.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + channel + "?callback=?", function(data) {
+  			console.log(data);
+	});
+	});
+}
+
 $(document).ready(function() {
+	callApi();
 });

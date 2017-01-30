7c08e267b753fd6200f760a3f4c7ff155a4224cf
Mandy Real <normandy.real@gmail.com>
2017-01-31 00:17:11 +1100

    testing twitchTV api

--- 02/twitch/js/app.js
+++ 02/twitch/js/app.js
@@ -1,12 +1,20 @@
-var channels = ["freecodecamp"]
+var channels = ["freecodecamp","ESL_SC2","measdfasd"]
 
 function callApi() {
 	var url = "https://wind-bow.gomix.me/twitch-api/streams/";
 
 	channels.forEach(function(channel){
+		// get channel status
+		$.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + channel + "?callback=?", function(data) {
+  			console.log(channel + ":");
+  			console.log(data.stream);
+		});
+		// get channel details
 		$.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + channel + "?callback=?", function(data) {
   			console.log(data);
-	});
+  			console.log("=====");
+		});
+
 	});
 }
 
